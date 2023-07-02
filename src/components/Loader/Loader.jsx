73fbd05a5d18a775b374bb90302d38c00200f338
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-07-02 18:42:35 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,19 +1,19 @@
 import { MutatingDots } from 'react-loader-spinner';
 
-function Loader() {
-  return (
-    <MutatingDots
-      height="100"
-      width="100"
-      color="#303f9f"
-      secondaryColor="#303f9f"
-      radius="12.5"
-      ariaLabel="mutating-dots-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-    />
-  );
-}
+  function Loader() {
+    return (
+      <MutatingDots
+        height="100"
+        width="100"
+        color="#303f9f"
+        secondaryColor="#303f9f"
+        radius="12.5"
+        ariaLabel="mutating-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    );
+  }
 
-export default Loader;
+  export default Loader;
